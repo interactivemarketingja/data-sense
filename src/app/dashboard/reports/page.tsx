f06c3f8f8ef70f6b4a6bd3f8db41a14573220954
Island Web Consultant <interactivemarketingja@gmail.com>
2025-11-13 03:01:21 +0000

    Try fixing this error: `Runtime ReferenceError: AlertDialogTrigger is no

--- src/app/dashboard/reports/page.tsx
+++ src/app/dashboard/reports/page.tsx
@@ -46,6 +46,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { useReports } from '@/context/reports-context'
 
