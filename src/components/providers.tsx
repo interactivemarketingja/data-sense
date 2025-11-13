2fe0f7e9d2cca3df2b7448a257da6938a304ea97
Island Web Consultant <interactivemarketingja@gmail.com>
2025-11-13 23:27:29 +0000

    Try fixing this error: `Console Error: Failed to load the PayPal JS SDK

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 export function Providers({ children }: { children: ReactNode }) {
-    const paypalClientId = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || "test";
+    const paypalClientId = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || "AVbxHI6gFTYy4L29yX2iwUNbConjdHbQYbB_FJnrMOXFbj93PM1AQgbcDytnYJsA8OfKCtMscEuD7b62";
 
     const initialOptions = {
         "client-id": paypalClientId,
